0da5ce5db7b3927549138f86957bc164f5efc518
Vishul Daby <itailormadeidiomas@gmail.com>
2026-02-22 21:28:48 -0300

    Fix database path and force table creation

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def create_app():
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     db.init_app(app)
-    with app.app_context(): db.create_all()
+    with app.app_context(): db.create_all()    with app.app_context(): db.create_all()
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
