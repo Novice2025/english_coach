fed2a95ea04860882834ea9ad5a10eba5e8198ce
Vishul Daby <itailormadeidiomas@gmail.com>
2026-02-23 18:09:28 -0300

    Final blueprint registration and clean init

--- app.py
+++ app.py
@@ -15,16 +15,20 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'english_coach.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+    
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(student_bp)
+    
     with app.app_context():
         db.create_all()
     return app
