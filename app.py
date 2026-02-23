ae1dc3fcbd7c883a23dcd0a2069f9e72b19d2f14
Vishul Daby <itailormadeidiomas@gmail.com>
2026-02-23 17:28:48 -0300

    Registering admin and student blueprints

--- app.py
+++ app.py
@@ -10,32 +10,23 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev_key_123')
-    
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'english_coach.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     db.init_app(app)
-
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-
-    # Register ALL Blueprints so the links in base.html work
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(student_bp)
-
     with app.app_context():
         db.create_all()
-
     return app
 
 if __name__ == '__main__':
