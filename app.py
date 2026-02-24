b3e12b52a50caee8b6b64346676bcb6276427883
Vishul Daby <itailormadeidiomas@gmail.com>
2026-02-24 12:17:59 -0300

    STABILIZE: Minimum viable product

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
 from flask import Flask
-from database.db import db
-from flask_login import LoginManager
 import os
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev_key_123')
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'english_coach.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    db.init_app(app)
-    
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-    
-    from models.user import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-    
-    from routes.auth import auth_bp
-    from routes.main import main_bp
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
-    
-    with app.app_context():
-        db.create_all()
-    return app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev_key_123'
+
+@app.route('/')
+def home():
+    return "<h1>VISHULCOACH IS ONLINE</h1><p>If you see this, the server is fixed.</p>"
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
-# Triggering fresh build
