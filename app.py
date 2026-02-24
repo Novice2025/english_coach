baa36e2e0401a3837f700da9f2a88f6a20f60a32
Vishul Daby <itailormadeidiomas@gmail.com>
2026-02-24 17:54:30 -0300

    RESET: Removing factory pattern to fix Status 1

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-import os
 
-# We define 'app' globally so Gunicorn can find it easily
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev_key_123'
 
 @app.route('/')
 def home():
     return "<h1>VISHULCOACH IS ONLINE</h1><p>The core engine is working.</p>"
 
-# This is for local testing, Render uses the 'app' object above
 if __name__ == '__main__':
     app.run(debug=True)
